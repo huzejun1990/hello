86359ebaf68db17c35bb0cffe9db0616e6e656bc
huzejun <961639130@qq.com>
2024-06-23 20:13:41 +0800

    Go入门教程-2024版 07-函数 2024-6-23 20:13:30

--- main.go
+++ main.go
@@ -97,17 +97,99 @@ func main() {
 		*p = 20
 		fmt.Println(a)*/
 
-	var p *int
+	//var p *int
 	//var a int = 10
 	//p = &a
 	//new函数 初始化一个内存空间 申请了一个内存空间，自然就有地址
 	//放的什么值? 对应类型的零值
-	p = new(int)
+	/*	p = new(int)
 	*p = 20
-	fmt.Println(p)
+	fmt.Println(p)*/
 	//fmt.Println("p是指针，它是一个地址：", p)
 	//fmt.Println("取p指向内存空间的值：", *p)
+
+	//声明一个函数
+	/*	var swap func(x, y int) (int, int)
+		swap = func(x, y int) (int, int) {
+			return y, x
+		}
+		fmt.Println(swap(10, 20))*/
+	//swap(1, 2.3, 4.5, 6.7)
+	/*	var a = 10
+		var b = 20
+		changeValue(a, b)
+		fmt.Println(a, b)
+
+		changeValue1(&a, &b)
+		fmt.Println(a, b)*/
+
+	/*	//这是一个匿名函数
+		funA := func() int {
+			return 20
+		}
+		//其实在这里funa就是函数的名字
+		funA()
+		//这是一个匿名函数调用 可以不用将函数声明为一个变量在使用
+		func() {
+			fmt.Println("这是一个匿名函数")
+		}()
+	*/
+
+	//闭包
+
+	//创建一个玩家生成器
+	generator := playerGen("战神")
+	//返回玩家的名字和血量
+	name, hp := generator()
+	//打印值
+	fmt.Println(name, hp)
+
 }
+
+func playerGen(name string) func() (string, int) {
+	//血量一直为150
+	hp := 150
+	//返回创建的闭包
+	return func() (string, int) {
+		//将变量引用到闭包中
+		return name, hp
+	}
+}
+
+/*func yes() (int, int, string) {
+	var a int
+	var b int
+	var c string
+	return a, b, c
+}*/
+
+/*func yes() (a int, b int, c string) {
+	a = 10
+	b = 20
+	c = "hello"
+	return
+}*/
+
+// 虽然是值传递，但是传递的是地址 修改的时候 还是原来a的内存空间，所以工受影响了
+// 引用传递 指针占用的空间 4字节 或者 8字节
+/*func changeValue1(x *int, y *int) {
+	*x = 30
+	*y = 50
+}
+
+func changeValue(x int, y int) {
+	x = 30
+	y = 50
+}
+
+// ...不固定参数 数组
+func swap(x int, arr ...float64) {
+	fmt.Println(x, arr)
+}
+*/
+/*func swap(x, y int) {
+
+}*/
 
 /*func getValue() {
 	var i = 10
