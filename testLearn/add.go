c637e9e018ad5022bba48c8e0f1a7993dfc2e63e
huzejun <961639130@qq.com>
2024-07-08 20:53:41 +0800

    Go入门教程-2024版 19-测试-基准测试 2024-7-8 20:53:30

--- testLearn/add.go
+++ testLearn/add.go
@@ -1,6 +1,31 @@
 // @Author huzejun 2024/7/7 20:47:00
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 func add(a, b int) int {
 	return a + b
+}
+
+func newSlice(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	//注意，这里在生成切片的时候并没有指定容量
+	nums := make([]int, 0)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func newSliceWithCap(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	//注意，这里生成切片的时候指定了容量
+	nums := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
 }
--- testLearn/add_test.go
+++ testLearn/add_test.go
@@ -1,5 +1,5 @@
 // @Author huzejun 2024/7/7 20:48:00
-package main
+/*package main
 
 import "testing"
 
@@ -39,5 +39,60 @@ func Test_add(t *testing.T) {
 				t.Errorf("add() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+// 这是一个测试辅助函数，它设置了测试需要的一些初始条件
+func setupTest(t *testing.T) (int, int) {
+	t.Helper() //标记这个函数为测试辅助函数
+	//这里可以是任何设置测试环境的代码
+	t.Errorf("setupTest error")
+	return 10, 20 //例如，返回两个用于测试的整数
+}
+
+// 这是一个测试函数
+func TestAdd(t *testing.T) {
+	a, b := setupTest(t) //调用测试辅助函数来设置测试数据
+	got := add(a, b)
+	want := 30
+	if got != want {
+		t.Errorf("Add(%d,%d) = %d; want %d", a, b, got, want)
+	}
+}
+*/
+
+package main
+
+import "testing"
+
+//func BenchmarkSprintf(b *testing.B) {
+//	num := 10
+//	//重置计时器，确保从一个一致的状态开始
+//	//基准测试函数 必须
+//	//在真正执行时重置，可以忽略准备期的开销
+//	b.ResetTimer()
+//	//b.N就是执行次数
+//	for i := 0; i < b.N; i++ {
+//		fmt.Sprintf("%d", num)
+//	}
+//}
+//
+//func BenchmarkFormat(b *testing.B) {
+//	num := int64(10)
+//	b.ResetTimer()
+//	for i := 0; i < b.N; i++ {
+//		strconv.FormatInt(num, 10)
+//	}
+//}
+
+func BenchmarkNewSlice(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		newSlice(1000000)
+	}
+}
+
+func BenchmarkNewSliceWithCap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		newSliceWithCap(1000000)
 	}
 }
